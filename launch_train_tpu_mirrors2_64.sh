98e4878539cb1e31eaa02929516257b887880ad8
dami sim c <dami.sim.c@gmail.com>
2019-11-15 11:08:47 -0600

    add training script to use gdrive directories

---
+++ launch_train_tpu_mirrors2_64.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+nohup pipenv run python3.6 main_tpu.py --use-tpu \
+	--train-input-path content/gdrive/My\ Drive/Artificial/images/datasets/mirrors2_4 \
+	--eval-input-path content/gdrive/My\ Drive/Artificial/images/datasets/mirrors2_4 \
+	--model-dir content/gdrive/My\ Drive/Artificial/images/models \
+	--result-dir .content/gdrive/My\ Drive/Artificial/images/results \
+	--batch-size 256  \
+	--ch 64 \
+	--self-attn-res 64 \
+	--g-lr 0.0001 \
+	--d-lr 0.0004 \
+	--verbosity INFO \
+	--train-examples 1281167 \
+	--eval-examples 50000 \
+	--tag sagan \
+	--tag run-$RANDOM \
+	$@ &
+	
